7ac9b94157723eb37540e435d549cee361bd9efc
Irene <irene.penuelas445@west-mec.org>
2017-05-17 14:49:19 -0700

    jQuery for onhover over thumbImg
    jquery and how it works to make the thumbnail images fade out and the party name to appear

--- scratchCapstone/js/functions.js
+++ scratchCapstone/js/functions.js
@@ -2,3 +2,13 @@
 $(document).ready(function(){
     $("#myCarousel").carousel({interval: 3000});
 });
+
+//fade thumbnail images and display partyName overlay
+  $(document).ready(function() {    
+    $(".thumbImg").on("mouseenter", function() {  // Initiate a function when the mouse enters an element with the class thumbImg
+      $(this).css("opacity", 0.3).next().show().css("color", "black");  // Fade out the img and show the partyName
+    });
+    $(".thumbImg").on("mouseout", function() {  // Once the mouse leaves an element with the class .thumbImg, initiate function hiding everything again
+      $(this).css("opacity", 1).next().hide();
+    });
+  });
