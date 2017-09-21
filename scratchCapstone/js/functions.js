d7fbd8b271fd1ac920618a6c09dec87968f2813a
Irene Penuelas <Ibarraza445@west-mec.org>
2017-09-21 15:31:53 -0700

    Halfway through creating validation for the login
    Added createEventListeners function and a function called validateLogin
    that is called once the sign in button is clicked. Still incomplete

--- scratchCapstone/js/functions.js
+++ scratchCapstone/js/functions.js
@@ -1,3 +1,6 @@
+/* Global Variables */
+var formValidity = true;
+
 /* Changing the time interval for the carousel */
 $(document).ready(function(){
     $("#myCarousel").carousel({interval: 3000}); // change image every 3 seconds
@@ -9,31 +12,71 @@ function fade(evt) {
   var target = evt.srcElement.id.split(" "); // Get id from whichever thumbImg triggered the event and split it so "pic #" -> "pic", "#"
   var targetDiv = document.getElementById("div " + target[1]); // Get corresponding div containing the party name by the second part of the broken up string with the number and attaching it to a "div " which all elements have
   targetDiv.style.display = "inline"; // show div with party name
-}
+} // ./fade()
 
 /* Have the party name disappear when thumbnail image is no longer hovered over */
 function fadeOut(evt) {
   var target = evt.srcElement.id.split(" "); // Get id from whichever thumbImg triggered the event and split it so "pic #" -> "pic", "#"
   var targetDiv = document.getElementById("div " + target[1]); // Get corresponding div containing the party name by the second part of the broken up string with the number and attaching it to a "div " which all elements have
   targetDiv.style.display = "none"; // hide div with party name
+} // ./fadeOut()
+
+function validateLogin() {
+  /* Stops the form from being submitted by default */
+  if (evt.preventDefault) { // for modern browsers
+    evt.preventDefault();
+  } else { // for older browser
+    evt.returnValue = false;
+  }
+  formValidity = true;
 }
 
-/* Event listeners for the thumbnail image elements */
-var thumbImg = document.getElementsByClassName("thumbImg"); // calling array of the thumbnail images
-var divArray = document.getElementsByClassName("partyName"); // calling array of divs containing party names
-for (var i = 0; i < thumbImg.length; i++) {
-  if(thumbImg[i].addEventListener) { // For Mozilla
-    thumbImg[i].addEventListener("mouseover", fade, false) // on hover thumbnail image, go to fade()
-    thumbImg[i].addEventListener("mouseleave", fadeOut, false) // not on hover thumbnail image, go to fadeOut()
-
-    divArray[i].addEventListener("mouseover", fade, false)  // on hover div element, go to fade()
-    divArray[i].addEventListener("mouseleave", fadeOut, false) // not on hover div element, go to fadeOut()
+function createEventListeners() {
+  /* Event Listener for login form */
+  var loginForm = document.getElementsByTagName("form")[0];
+  if (loginForm.addEventListener) { // For Mozilla
+    loginForm.addEventListener("submit", validateLogin, false);
   }
-  else if (thumbImg[i].attachEvent) { // For IE
-    thumbImg[i].attachEvent("onmouseleave", fade);  // on hover thumbnail image, go to fade()
-    thumbImg[i].attachEvent("onmouseleave", fadeOut); // not on hover thumbnail image, go to fadeOut()
+  else if (loginForm.attachEvent) { // For IE
+    loginForm.attachEvent("onsubmit", validateLogin);
+  }
 
-    divArray[i].attachEvent("onmouseleave", fade);  // on hover div element, go to fade()
-    divArray[i].attachEvent("onmouseleave", fadeOut); // not on hover div element, go to fadeOut()
-  } // ./else if
-} // ./for loop
+  /* Event listeners for the thumbnail image elements */
+  var thumbImg = document.getElementsByClassName("thumbImg"); // calling array of the thumbnail images
+  var divArray = document.getElementsByClassName("partyName"); // calling array of divs containing party names
+  for (var i = 0; i < thumbImg.length; i++) {
+    if(thumbImg[i].addEventListener) { // For Mozilla
+      thumbImg[i].addEventListener("mouseover", fade, false) // on hover thumbnail image, go to fade()
+      thumbImg[i].addEventListener("mouseleave", fadeOut, false) // not on hover thumbnail image, go to fadeOut()
+
+      divArray[i].addEventListener("mouseover", fade, false)  // on hover div element, go to fade()
+      divArray[i].addEventListener("mouseleave", fadeOut, false) // not on hover div element, go to fadeOut()
+    }
+    else if (thumbImg[i].attachEvent) { // For IE
+      thumbImg[i].attachEvent("onmouseleave", fade);  // on hover thumbnail image, go to fade()
+      thumbImg[i].attachEvent("onmouseleave", fadeOut); // not on hover thumbnail image, go to fadeOut()
+
+      divArray[i].attachEvent("onmouseleave", fade);  // on hover div element, go to fade()
+      divArray[i].attachEvent("onmouseleave", fadeOut); // not on hover div element, go to fadeOut()
+    } // ./else if
+  } // ./for loop
+
+  // /* Event listener for sign in button */
+  // var loginBtn = document.getElementById("loginBtn");
+  // if (loginBtn.addEventListener) { // For Mozilla
+  //   console.log("in if");
+  //   loginBtn.addEventListener("submit", login, false);
+  // }
+  // else if (loginBtn.attachEvent) { // For IE
+  //   console.log("in else");
+  //   loginBtn.attachEvent("onsubmit", login);
+  // }
+} // ./createEventListeners()
+
+/* On load, go to createEventListeners() */
+if (window.addEventListener) { // For Mozilla
+  window.addEventListener("load", createEventListeners, false);
+}
+else if (window.attachEvent) { // For IE
+  window.attachEvent("onload", createEventListeners);
+}
