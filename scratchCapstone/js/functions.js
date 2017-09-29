1791d0acaa29d2014921e4904c4464cfd3a919b4
Irene Penuelas <Ibarraza445@west-mec.org>
2017-09-29 15:31:22 -0700

    Sign-in inside nav working
    If there is data put in the boxes, then the two input boxes and buttons
    will disappear and a message saying “Welcome, user” appears in their
    place. If no data, boxes go red and a error message appears

--- scratchCapstone/js/functions.js
+++ scratchCapstone/js/functions.js
@@ -2,8 +2,10 @@
 var formValidity = true;
 
 /* Changing the time interval for the carousel */
-$(document).ready(function(){
-    $("#myCarousel").carousel({interval: 3000}); // change image every 3 seconds
+$(document).ready(function() {
+  $("#myCarousel").carousel({
+    interval: 3000
+  }); // change image every 3 seconds
 });
 
 /* IJPB 9.18.17 - fixed bug by removing broken jQuery code and replacing it with the function below */
@@ -21,23 +23,74 @@ function fadeOut(evt) {
   targetDiv.style.display = "none"; // hide div with party name
 } // ./fadeOut()
 
-function validateLogin() {
+function loginSuccess() {
+  var loginElements = document.getElementsByName("loginInfo");
+  var loginSuccess = document.getElementById("welcomeUser");
+  var navElements = document.getElementsByTagName("form")[0];
+
+  /* Make login elements disappear */
+  for (var i = 0; i < loginElements.length; i++) {
+    loginElements[i].style.display = "none";
+  }
+
+  /* Position nav elements to the right */
+  navElements.style.position = "relative";
+  navElements.style.left = "75%";
+  navElements.style.float = "right";
+  /* Display greeting */
+  loginSuccess.style.display = "inline-block";
+  loginSuccess.innerHTML = "Welcome, user!";
+
+} // ./loginSuccess()
+
+function validateLogin(evt) {
+  var errorMsg = document.getElementById("wrongLogin");
+  var loginInputs = document.getElementsByClassName("loginInput");
+
   /* Stops the form from being submitted by default */
   if (evt.preventDefault) { // for modern browsers
     evt.preventDefault();
   } else { // for older browser
     evt.returnValue = false;
   }
+
   formValidity = true;
-}
+
+  /* Check IF BOXES ARE BLANK */
+  for (var i = 0; i < loginInputs.length; i++) {
+    if (loginInputs[i].value === "") { // if blank
+      loginInputs[i].style.background = "rgb(205, 99, 92)"; // make box background red
+      formValidity = false;
+    } // ./if statement
+  } // ./for loop
+
+  if (formValidity === true) {
+    loginSuccess();
+  }
+  // else { // if input is valid
+  //   console.log("in else");
+  //   loginSuccess();
+  //   formValidity = false;
+  // }
+
+  /* Validity test */
+  if (formValidity === true) { // form is valid
+    errorMsg.innerHTML = ""; // turn off error messages
+    errorMsg.style.display = "none"; // remove error div
+    //document.getElementsByTagName("form")[0].submit(); // submit
+  } else { // form is not valid
+    errorMsg.innerHTML = "Please fix the indicated problems and then login again"; // display custom error message
+    errorMsg.style.display = "block"; // error message displays
+    scroll(0, 0); // scroll screen up so user sees error message
+  } // ./else statement
+} // ./validateLogin()
 
 function createEventListeners() {
   /* Event Listener for login form */
   var loginForm = document.getElementsByTagName("form")[0];
   if (loginForm.addEventListener) { // For Mozilla
     loginForm.addEventListener("submit", validateLogin, false);
-  }
-  else if (loginForm.attachEvent) { // For IE
+  } else if (loginForm.attachEvent) { // For IE
     loginForm.attachEvent("onsubmit", validateLogin);
   }
 
@@ -45,38 +98,25 @@ function createEventListeners() {
   var thumbImg = document.getElementsByClassName("thumbImg"); // calling array of the thumbnail images
   var divArray = document.getElementsByClassName("partyName"); // calling array of divs containing party names
   for (var i = 0; i < thumbImg.length; i++) {
-    if(thumbImg[i].addEventListener) { // For Mozilla
+    if (thumbImg[i].addEventListener) { // For Mozilla
       thumbImg[i].addEventListener("mouseover", fade, false) // on hover thumbnail image, go to fade()
       thumbImg[i].addEventListener("mouseleave", fadeOut, false) // not on hover thumbnail image, go to fadeOut()
 
-      divArray[i].addEventListener("mouseover", fade, false)  // on hover div element, go to fade()
+      divArray[i].addEventListener("mouseover", fade, false) // on hover div element, go to fade()
       divArray[i].addEventListener("mouseleave", fadeOut, false) // not on hover div element, go to fadeOut()
-    }
-    else if (thumbImg[i].attachEvent) { // For IE
-      thumbImg[i].attachEvent("onmouseleave", fade);  // on hover thumbnail image, go to fade()
+    } else if (thumbImg[i].attachEvent) { // For IE
+      thumbImg[i].attachEvent("onmouseleave", fade); // on hover thumbnail image, go to fade()
       thumbImg[i].attachEvent("onmouseleave", fadeOut); // not on hover thumbnail image, go to fadeOut()
 
-      divArray[i].attachEvent("onmouseleave", fade);  // on hover div element, go to fade()
+      divArray[i].attachEvent("onmouseleave", fade); // on hover div element, go to fade()
       divArray[i].attachEvent("onmouseleave", fadeOut); // not on hover div element, go to fadeOut()
     } // ./else if
   } // ./for loop
-
-  // /* Event listener for sign in button */
-  // var loginBtn = document.getElementById("loginBtn");
-  // if (loginBtn.addEventListener) { // For Mozilla
-  //   console.log("in if");
-  //   loginBtn.addEventListener("submit", login, false);
-  // }
-  // else if (loginBtn.attachEvent) { // For IE
-  //   console.log("in else");
-  //   loginBtn.attachEvent("onsubmit", login);
-  // }
 } // ./createEventListeners()
 
 /* On load, go to createEventListeners() */
 if (window.addEventListener) { // For Mozilla
   window.addEventListener("load", createEventListeners, false);
-}
-else if (window.attachEvent) { // For IE
+} else if (window.attachEvent) { // For IE
   window.attachEvent("onload", createEventListeners);
 }
